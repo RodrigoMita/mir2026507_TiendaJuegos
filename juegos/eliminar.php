c095e7e6484df17abc99748687b100793b98e90c
RodrigoMita <mir2026507@est.univalle.edu>
2024-08-23 13:32:28 -0400

    actualizacion

--- juegos/eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-require '_db.php';
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $query = "DELETE FROM games WHERE id = $id";
-    mysqli_query($conexion, $query);
-    header('Location: crud.php');
-}
-?>
